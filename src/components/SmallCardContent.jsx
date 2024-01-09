62a34f6139ae18a55f1ab407cae7a7e80c29c28e
Larocca <Larocca@RIVPRO1.net>
2024-01-08 19:20:43 -0500

    Working on glowing lightsabers

--- src/components/SmallCardContent.jsx
+++ src/components/SmallCardContent.jsx
@@ -1,12 +1,38 @@
-function SmallCardContent({name, image}){
+// function SmallCardContent({name, image}){
 
+//     return (
+//         <div className="small-card">
+//             <img src={image} alt={name}></img>
+//             <div className="lightsaber">
+//                 <div className="lightsaber-saber"></div>
+//                 <div className="lightsaber-saber-hilt"></div>
+//             </div>
+//             <p>{name}</p>
+//         </div>
+//     )
+// }
+
+// export default SmallCardContent;
+
+import { useState } from 'react';
+
+function SmallCardContent({name, image}) {
+    const [isSelected, setIsSelected] = useState(false);
+  
+    const handleClick = () => {
+      setIsSelected(!isSelected);
+    }
+  
     return (
-        <div className="small-card">
-            <img src={image} alt={name}></img>
-            <div>-</div>
-            <p>{name}</p>
+      <div className={`small-card ${isSelected ? 'selected' : ''}`} onClick={handleClick}>
+        <img src={image} alt={name}></img>
+        <div className="lightsaber">
+          <div className={`lightsaber-saber ${isSelected ? 'glow' : ''}`}></div>
+          <div className="lightsaber-saber-hilt"></div>
         </div>
+        <p>{name}</p>
+      </div>
     )
-}
+  }
 
-export default SmallCardContent;
+  export default SmallCardContent;
--- src/data.js
+++ src/data.js
@@ -1,5 +1,8 @@
 // Characters
+import admiral_ackbar from './images/characters/ADMIRAL-ACKBAR.jpeg';
 import ahsoka from './images/characters/ahsoka.jpeg';
+import anakin_skywalker from './images/characters/anakin-skywalker.jpeg';
+import baylan_skoll from './images/characters/baylan_skoll.jpeg';
 import sabine from './images/characters/sabine.jpeg';
 import hera from './images/characters/hera.jpeg';
 import ezra from './images/characters/ezra.jpeg';
@@ -7,10 +10,25 @@ import ezra from './images/characters/ezra.jpeg';
 
 const characters = [
     {
+        name: 'Admiral Ackbar',
+        image: admiral_ackbar,
+        about: 'A veteran commander, Ackbar led the defense of his homeworld, Mon Cala, during the Clone Wars and then masterminded the rebel attack on the second Death Star at the Battle of Endor. Ackbar realized the rebels had been drawn into a trap at Endor, but adjusted, with his fleet buying valuable time for the attack to succeed. After the Battle of Endor, Ackbar became a Grand Admiral in the New Republic, winning many victories including the pivotal Battle of Jakku. He retired to Mon Cala, but was coaxed back into service with the Resistance by Leia Organa.'
+    },    
+    {
         name: 'Ahsoka Tano',
         image: ahsoka,
         about: 'Former Jedi Knight Ahsoka Tano once served as the Padawan learner to Jedi Anakin Skywalker during the Clone Wars. A respected leader and warrior attuned to the light side of the Force, Ahsoka grew into a formidable fighter before the Empire’s reign changed the course of galactic history. Although she walked away from the Jedi Order, she continued to stand up for those fighting for peace and justice in the galaxy long after the fall of the Republic.'
     },
+    {
+        name: 'Anakin Skywalker',
+        image: anakin_skywalker,
+        about: "Discovered as a slave on Tatooine by Qui-Gon Jinn and Obi-Wan Kenobi, Anakin Skywalker had the potential to become one of the most powerful Jedi ever. Some even believed he was the prophesied Chosen One who would bring balance to the Force. Always pushing the limits of his Jedi training, seeking to excel and live up to his reputation, Skywalker's passion often brought him into conflict with his mentor, Jedi Master Obi-Wan Kenobi. A hero of the Clone Wars, Anakin was caring and compassionate, but suffered from a deep fear of loss that would prove to be his downfall."
+    },
+    {
+        name: 'Baylan Skoll',
+        image: baylan_skoll,
+        about: 'Baylan Skoll is lightsaber-wielding mercenary for hire overseeing his apprentice Shin Hati. But make no mistake: although there was a time when Skoll knew the acclaimed Jedi Knight Anakin Skywalker, Baylan is no Jedi. Now allied to Morgan Elsbeth, the former Magistrate of Calodan, after the fall of the Jedi and the Empire, Skoll remains a shrewd fighter in search of power who believes that another war is inevitable.'
+    },   
     {
         name: 'Sabine Wren',
         image: sabine,
