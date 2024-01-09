6321e842227f9b521f649d25fbddf15f60287694
Larocca <Larocca@RIVPRO1.net>
2024-01-09 18:24:03 -0500

    Styled Lightsaber component

--- src/components/Lightsaber.jsx
+++ src/components/Lightsaber.jsx
@@ -1,8 +1,8 @@
 function Lightsaber({ isActive }) {
     return (
         <div className="lightsaber">
-            <div className={`lightsaber-saber ${isActive ? 'glow' : ''}`}></div>
-            {/* <div className="lightsaber-saber-hilt"></div> */}
+            <div className={`lightsaber-saber ${isActive ? 'glow lightsaber-saber-active' : ''}`}></div>
+            <div className={`lightsaber-saber-hilt ${isActive ? 'lightsaber-saber-hilt-active' : ''}`}></div>
         </div>
     )
 }
