f364bc7454efbabf6306e8ea7f259d64c3e9b9fe
Larocca <Larocca@RIVPRO1.net>
2024-01-15 15:42:01 -0500

    Added buttons so the user can select a category

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { mapItems } from './utils/mapFunctions';
 import { characters, creatures } from './data';
 import StarWarsCard from './components/StarWarsCard';
@@ -9,14 +9,19 @@ import './App.css';
 function App() {
   const [selectedItem, setSelectedItem] = useState(null);
   const [items, setItems] = useState(characters); 
-  // const [items, setItems] = useState(creatures); 
+  const [selectedCategory, setSelectedCategory] = useState(characters);
 
+  useEffect(()=>{
+    setSelectedItem(selectedCategory[0]);
+  },[selectedCategory])
+      
   return (
     <>
     <nav>STAR WARS</nav>
+    <button onClick={()=>{setSelectedCategory(characters)}}>characters</button>
+    <button onClick={()=>{setSelectedCategory(creatures)}}>creatures</button>
     <section className="small-card-container">
-      {mapItems(characters, StarWarsCard, setSelectedItem, selectedItem, SmallCardContent)}
-      {/* {mapItems(creatures, StarWarsCard, setSelectedItem, selectedItem, SmallCardContent)} */}
+      {mapItems(selectedCategory, StarWarsCard, setSelectedItem, selectedItem, SmallCardContent)}
     </section>
         <StarWarsCard 
             type="article" 
