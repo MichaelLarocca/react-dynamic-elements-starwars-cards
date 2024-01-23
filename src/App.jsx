21a19f64c9c7b34a45f4ecfb41bd17b26851ba0a
Larocca <Larocca@RIVPRO1.net>
2024-01-22 19:04:14 -0500

    Created a Menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { mapItems } from './utils/mapFunctions';
-import { characters, creatures, droids, locations, organizations, vehicles, weapons_and_tech } from './data';
+import { characters, creatures, droids, locations, organizations, vehicles, weapons_and_tech } from './data.js';
+import Menu from './components/Menu';
 import Navbar from './components/Navbar';
-// import ButtonMenu from './components/ButtonMenu';
 import StarWarsCard from './components/StarWarsCard';
 import SmallCardContent from './components/SmallCardContent';
 import LargeCardContent from './components/LargeCardContent';
@@ -12,6 +12,7 @@ function App() {
   const [selectedItem, setSelectedItem] = useState(null);
   const [items, setItems] = useState(characters); 
   const [selectedCategory, setSelectedCategory] = useState(characters);
+  const categories = { characters, creatures, droids, locations, organizations, vehicles, weapons_and_tech };
 
   useEffect(()=>{
     setSelectedItem(selectedCategory[0]);
@@ -20,16 +21,10 @@ function App() {
   return (
     <>
     <Navbar />
-    <menu>
-      <button onClick={()=>{setSelectedCategory(characters)}}>Characters</button>
-      <button onClick={()=>{setSelectedCategory(creatures)}}>Creatures</button>
-      <button onClick={()=>{setSelectedCategory(droids)}}>Droids</button>
-      <button onClick={()=>{setSelectedCategory(locations)}}>Locations</button>
-      <button onClick={()=>{setSelectedCategory(organizations)}}>Organizations</button>
-      <button onClick={()=>{setSelectedCategory(vehicles)}}>Vehicles</button>
-      <button onClick={()=>{setSelectedCategory(weapons_and_tech)}}>Weapons & Tech</button>
-    </menu>  
-    {/* <ButtonMenu /> */}
+    <Menu 
+      setSelectedCategory={setSelectedCategory} 
+      categories={categories}
+    />
     <section className="small-card-container">
       {mapItems(selectedCategory, StarWarsCard, setSelectedItem, selectedItem, SmallCardContent)}
     </section>
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+export default function Menu({ setSelectedCategory, categories }) {
+    return (
+        <menu>
+            {Object.keys(categories).map(category => (
+                <button key={category} onClick={() => setSelectedCategory(categories[category])}>
+                    {category.charAt(0).toUpperCase() + category.slice(1)}
+                </button>
+            ))}            
+        </menu>
+    )
+}
