154cd8d040f4398b0b0b9a6d957bd5c01dbc0b51
Larocca <Larocca@RIVPRO1.net>
2024-02-21 16:59:21 -0500

    Working YouTube video model

--- src/components/LargeCardContent.jsx
+++ src/components/LargeCardContent.jsx
@@ -5,14 +5,14 @@ function LargeCardContent({name, image, about, isSelected, youtubeId}) {
     return (
         <div className="large-card">
             <div>
-                <img src={image} alt={name}></img>
+                {!youtubeId && <img src={image} alt={name}></img>}
+                {youtubeId && <YouTube videoId={youtubeId} />}
             </div>
             <div className="about">
                 <Lightsaber isActive={true} />
                 <h1>{name}</h1>
                 <p>{about}</p>
             </div>
-            {youtubeId && <YouTube videoId={youtubeId} />}
         </div>    
     )
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,16 @@
 export default function Menu({ setSelectedCategory, categories }) {
     return (
         <menu>
-            {Object.keys(categories).map(category => (
+            {/* {Object.keys(categories).map(category => (
                 <button key={category} onClick={() => setSelectedCategory(categories[category])}>
-                    {/* {category.charAt(0).toUpperCase() + category.slice(1)} */}
                     {category === 'youtube' ? 'VIDEO' : category.charAt(0).toUpperCase() + category.slice(1)}
                 </button>
-            ))}            
+            ))}             */}
+            {Object.keys(categories).filter(category => category !== 'youtube').map(category => (
+                <button key={category} onClick={() => setSelectedCategory(categories[category])}>
+                    {category.charAt(0).toUpperCase() + category.slice(1)}
+                </button>
+            ))}               
         </menu>
     )
 }
