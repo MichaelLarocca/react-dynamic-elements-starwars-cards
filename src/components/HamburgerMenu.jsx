9f16c4387b5f437262d1bde4fcda84917e3a7323
Michael Jude Larocca <michaeljudelarocca@gmail.com>
2024-05-06 00:50:57 -0400

    Fixed dropdown menu in the Hambuger menu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -67,6 +67,7 @@ export default function HamburgerMenu({
 						{isDatabankVisible && (
 							<Menu
 								setSelectedCategory={setSelectedCategory}
+								setShowCarousel={setShowCarousel}
 								categories={categories}
 							/>
 						)}
