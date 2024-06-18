1ecf30af3cd03a0603e143359697cde275eb01aa
Larocca <Larocca@RIVPRO1.net>
2024-06-18 15:07:20 -0400

    News Component - working model

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,39 +1,56 @@
 import { news } from "../data.js";
 import BB8 from "../images/news/BB8.jpeg";
 
-function News({news, storyNumber}) {
+const Article = ({ content }) => {
+  return (
+    <div>
+      {content.map((item, index) => {
+        if (item.type === 'paragraph') {
+          return <p key={index}>{item.text}</p>;
+        } else if (item.type === 'image') {
+          return <img key={index} src={item.src} alt={item.alt} className="news__image-body" />;
+        }
+        return null;
+      })}
+    </div>
+  );
+};
+
+function News({ news, storyNumber }) {
+
+  const articleContent = [];
+
+  for (let key in news[storyNumber]) {
+    if (key.startsWith('paragraph')) {
+      articleContent.push({ type: 'paragraph', text: news[storyNumber][key] });
+    } else if (key.startsWith('image') && !news[storyNumber][key].includes('title')) {
+      articleContent.push({ type: 'image', src: news[storyNumber][key], alt: key });
+    }
+  }
+
   return (
     <div className="ctn-news">
       <img src={news[storyNumber].image} alt="Placeholder" className="news__image-main" />
       <div className="news__title">
-        <h1>{news[storyNumber].title}</h1>
+        <h1>{news[storyNumber].name}</h1>
         <h4>{news[storyNumber].sub_title}</h4>
       </div>
-      <div className="news__about"> 
+      <div className="news__about">
         <div className="ctn-news-header">
-          {/* <img src="src/images/news/BB-8.jpeg" width="50px" height="50px" alt="Placeholder" className="news__image-author" /> */}
           <img src={BB8} width="50px" height="50px" alt="Placeholder" className="news__image-author" />
           <div>
             <p className="ctn-news-author gray"><strong>By:</strong> {news[storyNumber].by}</p>
-            <p  className="ctn-news-date gray"><strong>date:</strong> {news[storyNumber].date}</p>
+            <p className="ctn-news-date gray"><strong>date:</strong> {news[storyNumber].date}</p>
           </div>
         </div>
         <div className="news__social-icons">
-          {/* Social icons go here */}
           <div>Social icons go here</div>
         </div>
       </div>
       <div className="news__content">
-        <p>
-        {news[storyNumber].paragraph_1}
-        </p>
-        <img src={news[storyNumber].image_1} alt="Placeholder" className="news__image-body"/>
-        <p>
-        {news[storyNumber].paragraph_2}
-        </p>
+        <Article content={articleContent} />
       </div>
       <div className="news__footer">
-        {/* Footer content goes here */}
         <footer><strong>StarWars.com. All Star Wars, all the time.</strong></footer>
       </div>
     </div>
@@ -41,4 +58,3 @@ function News({news, storyNumber}) {
 }
 
 export default News;
-
